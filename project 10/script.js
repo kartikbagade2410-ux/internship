84420b8a7796100e2b39f83127876b40ba8d1b9e
ganesh avsarmol <avsarmolganesh93@gmail.com>
2026-07-01 11:30:38 +0530

    final

---
+++ project 10/script.js
@@ -0,0 +1,12 @@
+document.querySelector("button").addEventListener("click",()=>{
+    document.querySelector("h1").innerText = "DOM Manipulation Learned!"
+})
+document.querySelector(".btn-1").addEventListener("click",()=>{
+    document.body.style.backgroundColor ="Red"
+})
+document.querySelector(".btn-2").addEventListener("click",()=>{
+    document.body.style.backgroundColor ="Green"
+})
+document.querySelector(".btn-3").addEventListener("click",()=>{
+    document.body.style.backgroundColor ="Blue"
+})
