c5a8cc13e10d56e86b76ec51e86f4bc5a1050341
ganesh avsarmol <avsarmolganesh93@gmail.com>
2026-07-02 11:19:34 +0530

    js code added

---
+++ project 11/script.js
@@ -0,0 +1,9 @@
+let name="kunal";
+let age="19;"
+let city="Nandura";
+let istsudent ="true";
+
+console.log("Hello my name is",name)
+console.log("I am",age,"years old")
+console.log("I live in",city)
+console.log("student",isstudent)
