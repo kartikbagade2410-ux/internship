9bc36215c032e583e2b28c14a478b0fa018ea184
kartik <kartikbagade2410@gmail.com>
2026-07-07 12:46:14 +0530

    completed

---
+++ PROJECT 1/script.js
@@ -0,0 +1,9 @@
+let name ="kunal";
+let age ="19";
+let city ="Nandura";
+let isstudent ="true";
+
+console.log("My name is",name);
+console.log(I )
+
+
---
+++ project 12/script.js
@@ -0,0 +1,11 @@
+let a=20;
+let b=6;
+
+console.log("Number1=",a);
+console.log("Number2=",b);
+
+console.log("Addition =",a + b);
+console.log("subterction =",a - b);
+console.log("mulliplication =",a * b);
+console.log("division =",a / b);
+console.log("remainder =",a % b);
