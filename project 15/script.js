09733a8dc86e9dd066119b191174f50d1080ffea
kartik <kartikbagade2410@gmail.com>
2026-07-15 12:34:45 +0530

    code complit

--- project 15/script.js
+++ project 15/script.js
@@ -1,10 +1,31 @@
-function initail(name){
-    let nameAt = name.split(" ");
-    let str = ""
-    nameAt.map((item,index)=>{
-        str = str + item[0].toUpperCase()
-    });
-console.log(str);
+// task 1
 
-}
-initail("Virat kohli");
+// function initail(name){
+//     let nameAt = name.split(" ");
+//     let str = ""
+//     nameAt.map((item,index)=>{
+//         str = str + item[0].toUpperCase()
+//     });
+// console.log(str);
+
+// }
+// initail("Virat kohli");
+
+// task 2
+
+// let num = 10;
+
+// function numberSqaure(num){
+//     console.log(Math.sqrt(num));
+//     console.log(Math.pow(num,3));
+//     console.log(Math.pow(num,2));
+//     if(num%2==0){
+//         console.log("even");
+        
+//     }else{
+//         console.log("odd");
+        
+//     }
+
+// }
+// numberSqaure(num)
