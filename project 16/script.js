c42f0072d1a2f0a1892419467d82762d98407622
kartik <kartikbagade2410@gmail.com>
2026-07-17 13:28:02 +0530

    complite code

---
+++ project 16/script.js
@@ -0,0 +1,118 @@
+// task 1
+
+
+// function initail(name){
+//     return name.split("").reverse().join("")
+// }
+// const name = "Javascript"
+// console.log(initail(name));
+
+// task 2
+
+// let name = "Programming"
+// function inetial(name){
+// return name.split("")
+// .filter((item) =>
+//        ( item=="a"||
+//          item=="e"||
+//           item=="i"||
+//            item=="o"||
+//             item=="u")
+//     ).length
+
+// }
+// console.log(inetial(name));
+
+// task 3
+
+// let name = ["Apple","Banana","Orange"]
+// function initail(name){
+//     if(name.includes("Banana")){
+//         console.log("Fruit found");
+        
+//     }
+//     else{
+//         console.log("Fruit nor found");
+        
+//     }
+// }
+// initail(name)
+
+// task 4
+
+// function rollDice() {
+
+//     let dice1 = Math.floor(Math.random() * 6) + 1;
+//      let dice2 = Math.floor(Math.random() * 6) + 1;
+
+//      console.log("Dice 1:",dice1);
+//      console.log("Dice 2:",dice2);
+//      console.log("Total:",dice1 + dice2);
+
+
+// }
+// rollDice();
+
+// task 5
+
+// function CurrentTime(){
+//     let time = new Date();
+//     console.log(`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`);
+// }
+// CurrentTime();
+
+// task 6
+
+// let remove = ["HTML","CSS","JAVASCRIPT","REACT"]
+//      function kartik(remove){
+//         remove.shift();
+//             remove.pop()
+//             console.log(remove);
+            
+        
+//      }
+//      kartik(remove);
+
+// task 7
+
+// let add =["INDIA","JAPAN","CANADA"]
+// function kartik(add){
+//     add.push("AUSTRALIA");
+//     console.log(add);
+    
+// }
+// kartik(add);
+
+// task 8
+
+// let find = [12,44,78,23,56]
+// function kartik(find){
+
+//     console.log(Math.max(12,44,78,23,56));
+    
+// }
+// kartik(find)
+
+// task 9
+
+// let count = ["PEN","SCALE","BOOK","ERASER"]
+// function kartik(count){
+//     console.log(count.length);
+    
+// }
+// kartik(count);
+
+
+
+
+// task 10
+
+// let name = "Kunal"
+// function kartik(){
+//     console.log(name.toUpperCase());
+//     console.log(name.length);
+//     console.log(`Wellcome ${name}`);
+    
+
+// }
+// kartik();
